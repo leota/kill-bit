50cad0c661545878d8db80ac88ed8d140c713530
Leonardo Maglio <leotagit@gmail.com>
2017-08-18 22:48:07 +0200

    logging starting balance

--- index.js
+++ index.js
@@ -1,53 +1,71 @@
 let schedule = require('node-schedule')
     colors = require('colors')
+    moment = require('moment')
     c = require('./lib/client')
     a = require('./lib/actions')
 
 /* GLOBAL VARIABLES */
-let assetAccoundID, currencyAccountID, lastTradeTime, lastTradePrice,
+let assetAccountID, currencyAccountID, startAssetCapital, startCurrencyCapital, lastTradeTime, lastTradePrice,
+asset = c.selector.split('-')[0]
+currency = c.selector.split('-')[1]
 btcToKeep = 0.00005
 eurToKeep = 1
 INTERVAL = 0.20 // EUR
 
-c.authedClient.getAccounts(function(err, response, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  // Get Asset and Currency accounts IDs
-  let asset = c.selector.split('-')[0]
-  currency = c.selector.split('-')[1]
-  for( let i = 0; i < data.length; i++) {
-  	if(data[i].currency == asset) {
-  		assetAccoundID = data[i].id;
-  	}
-  	if(data[i].currency == currency) {
-  		currencyAccountID = data[i].id;
-  	}
-  }
-  // Run trade task every 10 seconds
-  let jobScheduler = schedule.scheduleJob('*/10 * * * * *', function(){
-    trade(c.authedClient, c.publicClient, assetAccoundID, currencyAccountID);
-  });
-});
+a.getAccountsIDs(c.authedClient, c.selector).then( res => {
+  assetAccountID = res[0]
+  currencyAccountID = res[1]
+  return true
+}).catch(err => {
+  console.log("ERROR[getAccountsIDs]:".red, err)
+})
+.then( () => {
+  a.getAssetCapital(c.authedClient, assetAccountID).then( res => {
+    startAssetCapital = parseFloat(res.balance).toFixed(5)
+    return true
+  }).catch(err => {
+    console.log("ERROR[getAssetCapital]:".red, err)
+  })
+}).catch(err => {
+  console.log("ERROR[getAssetCapitalWrap]:".red, err)
+})
+.then( () => {
+  a.getCurrencyCapital(c.authedClient, currencyAccountID).then( res => {
+    startCurrencyCapital = parseFloat(res.balance).toFixed(2)
+    console.log("STARTING BALANCE:".green, startAssetCapital.yellow, asset.yellow, '---', startCurrencyCapital.yellow, currency.yellow)
+    return true
+  }).catch(err => {
+    console.log("ERROR[getCurrencyCapital]:".red, err)
+  })
+}).catch(err => {
+  console.log("ERROR[getCurrencyCapitalWrap]:".red, err)
+})
 
-function trade(authedClient, publicClient, assetAccoundID, currencyAccountID) {
+  // Run trade task every 10 seconds
+  // let jobScheduler = schedule.scheduleJob('*/10 * * * * *', function(){
+  //   trade(c.authedClient, c.publicClient, assetAccountID, currencyAccountID);
+  // });
+
+function trade(authedClient, publicClient, assetAccountID, currencyAccountID) {
   a.getLastTrade(publicClient).then(res => {
-    lastTradeTime = res.time
-    lastTradePrice = res.price
-    console.log(lastTradeTime.grey, lastTradePrice.cyan)
+    lastTradeTime = moment(res.time).format('DD-MM-YYYY HH:mm:ss')
+    lastTradePrice = parseFloat(res.price)
+    console.log(lastTradeTime.grey, lastTradePrice.toFixed(2).cyan)
   }).catch(err => {
     console.log("ERROR[getLastTradePrice]:".red, err)
   })
 }
 
+  // a.getCurrencyCapital(c.authedClient).then(res => {
+  //   console.log("curr cap",res)
+  // })
 
 /******** Trade Task Definition **********/
 
-function tradeTask(client, assetAccoundID, currencyAccountID) {
+function tradeTask(client, assetAccountID, currencyAccountID) {
 	//Get available funds from accounts
 	var btcFunds, eurFunds;
-	client.getAccount(assetAccoundID, function(err, response, data) {
+	client.getAccount(assetAccountID, function(err, response, data) {
 	  if (err) {
 	    console.log(err);
 	    return;
