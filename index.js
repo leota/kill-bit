6e303747457cbff57b4493a65aeb1d52bfe27bc6
Leonardo Maglio <leotagit@gmail.com>
2017-08-19 19:27:44 +0200

    panic buy/sell implemented

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ let schedule = require('node-schedule')
     t = require('./lib/trade')
 
 /* GLOBAL VARIABLES */
-let assetAccountID, currencyAccountID, startAssetCapital, startCurrencyCapital, 
-    lastTradeTime, lastTradePrice, lastFill, lastFillPrice,
+let assetAccountID, currencyAccountID, startAssetCapital, startCurrencyCapital,
     asset = c.selector.split('-')[0]
     currency = c.selector.split('-')[1]
-    PANIC = 50;
-    btcToKeep = 0.00005
-    eurToKeep = 1
-    INTERVAL = 0.20 // EUR
+    PANIC = 50
+    MARGIN = 10
+    DROP = 2
 
 /* EXECUTE ONLY ONCE: Get Account balances */
 a.getAccountsIDs(c.authedClient, c.selector).then( res => {
@@ -45,98 +43,6 @@ a.getAccountsIDs(c.authedClient, c.selector).then( res => {
   console.log("ERROR[getCurrencyCapitalWrap]:".red, err)
 }).then(() => {
    let jobScheduler = schedule.scheduleJob('*/10 * * * * *', function(){
-      t.trade(c.authedClient, c.publicClient, assetAccountID, currencyAccountID);
+      t.trade(c.authedClient, c.publicClient, assetAccountID, currencyAccountID, c.selector);
   });
 })
-
-
-
-/******** Trade Task Definition **********/
-
-function tradeTask(client, assetAccountID, currencyAccountID) {
-	//Get available funds from accounts
-	var btcFunds, eurFunds;
-	client.getAccount(assetAccountID, function(err, response, data) {
-	  if (err) {
-	    console.log(err);
-	    return;
-	  }
-	  btcFunds = parseFloat(data.available);
-	  client.getAccount(currencyAccountID, function(err, response, data) {
-	    if (err) {
-	      console.log(err);
-	      return;
-	    }
-	    eurFunds = parseFloat(data.available);
-	    console.log("$$$ Available Funds $$$");
-	    console.log("BTC: ", btcFunds.toFixed(5));
-	    console.log("EUR: ", eurFunds.toFixed(5));
-      console.log("$$$$$$$$$$$$$$$$$$$$$$$");
-    	//Get list of open orders
-    	client.getOrders(function(err, response, data) {
-            if (err) {
-                console.log("[getOrders]: ", err);
-                return;
-            }
-            console.log("Current open orders: ", data);
-            ordersLength = data.length;
-            // If there is no open orders
-            if(ordersLength <= 0) {
-            	// Get list of fills
-            	client.getFills(function(err, response, data) { /* TODO: get paginated fills*/
-            	    if (err) {
-            	        console.log(err);
-            	        return;
-            	    }
-            	    if(data.length <= 0) { /* Alert if no fills are available */
-            	    	console.log("!!! Please place a manual order to begin auto-trading !!!")
-            	    	return;
-            	    }
-            	    var latestFill = data[0];
-            	    console.log("---> Latest fill: ", latestFill);
-            	    // START PLACING orders
-            	    if(latestFill.side == 'buy') {
-            	    	var limitPrice = (parseFloat(latestFill.price) + INTERVAL).toFixed(2);
-                    var size = (btcFunds - btcToKeep).toFixed(8);
-                    var args = {
-                      price: limitPrice.toString(), // EUR
-                      size: size.toString(),  // BTC
-                      product_id: 'BTC-EUR',
-                      post_only: true
-                    };
-                    authedClient.sell(args, function(err, response, data) {
-                      if (err) {
-                        console.log(err);
-                        return;
-                      }
-                      console.log("<--- SELL order placed: ", data);
-                    });
-            	    }
-            	    if(latestFill.side == 'sell') {
-            	    	var limitPrice = (parseFloat(latestFill.price) - INTERVAL).toFixed(2);
-            	    	var size = ((eurFunds - eurToKeep) / limitPrice).toFixed(8);
-            	    	var args = {
-            	    	  price: limitPrice.toString(), // EUR
-            	    	  size: size.toString(),  // BTC
-            	    	  product_id: 'BTC-EUR',
-                      post_only: true
-            	    	};
-            	    	authedClient.buy(args, function(err, response, data) {
-            	    	  if (err) {
-            	    	    console.log(err);
-            	    	    return;
-            	    	  }
-            	    	  console.log("<--- BUY order placed: ", data);
-            	    	});
-            	    }
-            	});
-            }
-            // else try again
-            else {
-            	console.log("---> THERE ARE STILL OPEN ORDERS <---");
-            	return;
-            }
-        });
-	  });
-	});
-}
