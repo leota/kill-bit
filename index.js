b5340e87d82e49b57a62ccc62dbf7c83e6256611
Leonardo Maglio <leotagit@gmail.com>
2017-08-23 00:10:25 +0200

    refactor and RETRY mode

--- index.js
+++ index.js
@@ -9,55 +9,41 @@ let schedule = require('node-schedule')
 let assetAccountID, currencyAccountID, startAssetCapital, startCurrencyCapital, lastTradePrice, startTotal,
     asset = c.selector.split('-')[0]
     currency = c.selector.split('-')[1]
-    PANIC = 10
+    
     MARGIN = 2
     DROP = 0.5
-    BUY_AMOUNT = 70
-    SELL_AMOUNT = 0.02
+    PANIC = 40
+    RETRY = 10
+
 
 /* EXECUTE ONLY ONCE: Get Account balances */
 a.getAccountsIDs(c.authedClient, c.selector).then( res => {
   assetAccountID = res[0]
   currencyAccountID = res[1]
-  return true
-}).catch(err => {
-  console.log("ERROR[getAccountsIDs]:".red, err)
-})
-.then( () => {
+  /* Get asset capital */
   a.getAssetCapital(c.authedClient, assetAccountID).then( res => {
     startAssetCapital = parseFloat(res.balance).toFixed(5)
-    return true
+    /* Get currency capital */
+    a.getCurrencyCapital(c.authedClient, currencyAccountID).then( res => {
+      startCurrencyCapital = parseFloat(res.balance).toFixed(2)
+      /* Get last trade price*/
+      a.getLastTrade(c.publicClient).then(res => {
+        lastTradePrice = parseFloat(res.price)
+        startTotal = parseFloat((parseFloat(startAssetCapital) * lastTradePrice) + parseFloat(startCurrencyCapital)).toFixed(2)
+        /* EXECUTE every 30 seconds */
+        let jobScheduler = schedule.scheduleJob('*/30 * * * * *', function(){
+            console.log("STARTING BALANCE:".green, startAssetCapital.gray, asset.gray, '---', startCurrencyCapital.gray, currency.gray, '--->', startTotal.yellow, currency.yellow)
+            t.trade(c.authedClient, c.publicClient, assetAccountID, currencyAccountID, c.selector, startTotal);
+        });
+      }).catch(err => {
+        console.log("ERROR[getLastTradePrice]:".red, err)
+      })
+    }).catch(err => {
+      console.log("ERROR[getCurrencyCapital]:".red, err)
+    })
   }).catch(err => {
     console.log("ERROR[getAssetCapital]:".red, err)
   })
 }).catch(err => {
-  console.log("ERROR[getAssetCapitalWrap]:".red, err)
-})
-.then( () => {
-  a.getCurrencyCapital(c.authedClient, currencyAccountID).then( res => {
-    startCurrencyCapital = parseFloat(res.balance).toFixed(2)
-    return true
-  }).catch(err => {
-    console.log("ERROR[getCurrencyCapital]:".red, err)
-  })
-}).catch(err => {
-  console.log("ERROR[getCurrencyCapitalWrap]:".red, err)
-})
-.then( () => {
-  /* Get last trade price*/
-  a.getLastTrade(c.publicClient).then(res => {
-    lastTradePrice = parseFloat(res.price)
-    startTotal = parseFloat((parseFloat(startAssetCapital) * lastTradePrice) + parseFloat(startCurrencyCapital)).toFixed(2)
-  }).catch(err => {
-    console.log("ERROR[getLastTradePrice]:".red, err)
-  })
-}).catch(err => {
-  console.log("ERROR[getLastTradePriceWrap]:".red, err)
-})
-.then(() => {
-    /* EXECUTE every 10 seconds */
-   let jobScheduler = schedule.scheduleJob('*/10 * * * * *', function(){
-      console.log("STARTING BALANCE:".green, startAssetCapital.gray, asset.gray, '---', startCurrencyCapital.gray, currency.gray, '--->', startTotal.yellow, currency.yellow)
-      t.trade(c.authedClient, c.publicClient, assetAccountID, currencyAccountID, c.selector, startTotal);
-  });
+  console.log("ERROR[getAccountsIDs]:".red, err)
 })
